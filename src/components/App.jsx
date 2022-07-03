67e9efbb8c082f97d06c5fefcd55c1daf58f8eb5
Ritin George <ritingeorge.d@gmail.com>
2022-07-03 18:06:25 +0530

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,22 @@ import styles from './App.module.css'
 export default function App() {  
   const [to, setTo] = useState([]);
   const [toInput, setToInput] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
   const [subjectInput, setSubjectInput] = useState("");
-  const [html,setHtml] = useState('<div class="example"> Hello World Email! </div>')
+  const [html,setHtml] = useState('<div class="example"> Type you HTML here! </div>')
   const [css,setCss] = useState(
   `.example {
     color: red;
   }
   `)
   const toInputRef = useRef(null);
+  const topRef = useRef(null);
 
   const sendMail = (to, sub, htmlSrc)=> {
+    topRef.current.scrollIntoView()
+    if(to.length == 0) return setError(" Please Enter a To Address!");
+    console.log(htmlSrc);
     fetch("https://shn-3-gmail-api.vercel.app/api/sendMail", {
         method: "POST",
         body: JSON.stringify({
@@ -29,6 +35,17 @@ export default function App() {
       })
       .then(response => response.json())
       .then(data =>{
+        if(data) { 
+        setSuccess("Mail sent successfully! check your spam folder too.")
+        setError(null);
+        setSubjectInput("");
+        setHtml(`<div class="example"> Type your HTML here! </div>`)
+        setCss(`
+        .example {
+          color: red;
+        }
+        `)
+        }
       }).catch((err)=> console.log(err))
   }
 
@@ -48,17 +65,31 @@ export default function App() {
     
     const srcDoc = `
     <html>
+      <head>
+        <style>${css}</style>
+      </head>
       <body>${html}</body>
-      <style>${css}</style>
     </html>
     `
     
     return (
         <>
-        <div className="flex max-w-screen flex-col">
+        <div ref={topRef} className="flex max-w-screen mx-5 flex-col items-center">
           <div className='w-full '>
-          <h1>To</h1>
-          <div onClick={()=> toInputRef.current.focus} className={`${styles.textInput} w-full bg-slate-50 border-2 flex items-center pl-2 h-9 rounded-lg`}>
+          <h1 className='text-6xl text-center font-bold pt-3'>INFLATED EMAILS</h1>
+          <h1 className='text-3xl text-center '>Make your own custom e-mails ;&#41;</h1>
+          {error ?
+            (
+              <p className='font-bold pt-5 text-center text-red-600'>{error}</p>
+            ) : null
+          }
+          {success ?
+            (
+              <p className='font-bold pt-5 text-center text-lime-500'>{success}</p>
+            ) : null
+          }
+          <h1 className="font-bold	pt-3">To</h1>
+          <div onClick={()=> toInputRef.current.focus} className={`${styles.textInput} w-full bg-slate-50 border-2 border-black flex items-center pl-2 h-9 rounded-lg`}>
               {to? <div className="flex text-center cursor-pointer items-center gap-2 ml-2 py-1">
                 {to.map((to, index)=>(
                   <div key={index} onClick={()=>{removeTo(index)}} className="bg-red-300 px-3 rounded-lg">
@@ -68,12 +99,13 @@ export default function App() {
                 )}</div> : null}
               <input style={{"all": "unset", "paddingLeft": "5px", "width": "100%"}} ref={toInputRef}  type="text"  value={toInput} onChange={(e)=> setToInput(e.target.value)} />
             </div>
-          </div>
-          <h1>Subject</h1>
-          <input className='w-full bg-slate-50 border-2 flex items-center pl-2 h-9 rounded-lg' type="text" onChange={e => {
+            <h1 className="font-bold pt-3">Subject</h1>
+          <input className='w-full border-black bg-slate-50 border-2 flex items-center pl-2 h-9 rounded-lg' type="text" onChange={e => {
             e.preventDefault()
             setSubjectInput(e.target.value)
             }} name="subject" id="sub" />
+          </div>
+
           <div className='h-full w-full'>
           <div className=" w-full gap-y-10 flex md:flex-col">
             <MonacoEditor
@@ -88,9 +120,9 @@ export default function App() {
             />
           </div>
           <div className='w-full'>
-          <h1>PREVIEW</h1>
+          <h1 className="font-bold text-center py-5">PREVIEW</h1>
               <iframe 
-                  className='border-2'
+                  className="border-4 border-black rounded-lg"
                   srcDoc={srcDoc}
                   title='output'
                   sandbox='allow-scripts'
@@ -100,7 +132,8 @@ export default function App() {
               />
           </div>
           </div>
-          <button onClick={()=>sendMail(to, subjectInput, srcDoc)}>SEND</button>
+          <button className="font-bold border-2 border-black bg-lime-500 my-5 px-5 w-[200px] rounded-lg py-2 hover:scale-105 hover:bg-lime-600 hover:text-white" onClick={()=>sendMail(to, subjectInput, srcDoc)}>SEND</button>
+          <div className='pt-1 pb-4'>Created with 💙 by inflated papadam</div>
         </div>
         </>
   )
