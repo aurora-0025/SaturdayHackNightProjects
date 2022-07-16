56f6f16b4f671e8842d4c81eeaf1f6d22c668d99
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-07-17 04:16:24 +0530

    Update Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,7 +2,7 @@ function Post({record}) {
 return (
     <div className='m-3 p-2 rounded-lg border border-3 border-blue-500 drop-shadow-lg shadow-sm text-left'>
       <div className='flex items-center mx-3'>
-        <img className='rounded-full w-[30px] h-[30px] border border-white border-3' src={record.fields.Image}/>
+        <img alt={record.fields.Email} className='rounded-full w-[30px] h-[30px] border border-white border-3' src={record.fields.Image}/>
         <div className='flex mx-2 flex-col items-start'>
           <h1 className='font-bold text-sm'>{record.fields.Name}</h1>
           <h2 className='text-gray-800 text-xs'>{record.fields.Email}</h2>
